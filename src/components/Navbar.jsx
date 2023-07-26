7cbe3a6618cc6ed2cad4de69cd4b0ec4c8668223
franco <francocatania93@gmail.com>
2023-07-26 20:35:38 -0300

    Terminada primera pre entrega

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import { CartWidget } from "./CartWidget"
 
 export const Navbar = () =>{
     return <nav className="nav">
-                <div className="nav__logoContainer">
+                <div className="nav__brandNameContainer">
                     Baboon Clothes
                 </div>
                 <ul className="nav__listaNav">
-                    <li className="nav__itemLista">Abrigos</li>
-                    <li className="nav__itemLista">Remeras</li>
-                    <li className="nav__itemLista">Calzado</li>
+                    <li className="nav__itemLista"><a href="#" className="nav__linkLista">Abrigos</a></li>
+                    <li className="nav__itemLista"><a href="#" className="nav__linkLista">Remeras</a></li>
+                    <li className="nav__itemLista"><a href="#" className="nav__linkLista">Calzado</a></li>
                 </ul>
                 <CartWidget></CartWidget>
             </nav>
