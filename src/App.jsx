b9142d0b388c23a27ce9a315fd681939b624bfd1
Franco Catania <francocatania93@gmail.com>
2023-08-22 18:34:40 -0300

    Se agregó localStorage al carrito y se creo el archivo netlify.toml para arreglar errores de enrutamiento en Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,19 @@
-import { useState } from 'react'
+
 import './App.scss'
 import { Navbar } from './components/Navbar'
 import { ItemListContainer } from './components/ItemListContainer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ItemDetail } from './components/ItemDetail'
 import { ItemDetailContainer } from './components/ItemDetailContainer'
-import { CartContext } from './context/CartContext'
+import { CartContext, CartProvider } from './context/CartContext'
 import { Carrito } from './components/Carrito'
 
 function App() {
-  const [carrito, setCarrito] = useState([])
-
-  const agregar1AlCarrito = (item, cantidad) =>{
-    const itemAgregado = {...item, cantidad}
-    const estaAgregado = carrito.find(prenda => prenda.id === itemAgregado.id)
-
-    estaAgregado ? (estaAgregado.cantidad += 1 , setCarrito([...carrito])) : setCarrito([...carrito, itemAgregado])
-  
-  }
-
-  const agregarAlCarrito = (item, cantidad) =>{
-    const nuevoCarrito = [...carrito]
-    const itemAgregado = {...item, cantidad}
-    const estaEnCarrito = nuevoCarrito.find(producto => producto.id === itemAgregado.id)
-
-    if(estaEnCarrito){
-        estaEnCarrito.cantidad += cantidad
-        setCarrito([...nuevoCarrito])
-    }
-    else {
-        setCarrito([...carrito, itemAgregado])
-    }   
-    
-  }
-  
-  const eliminarDeCarrito = (item) =>{
-    const index = carrito.findIndex(prenda => prenda.id === item.id)
-    const nuevoCarrito = [...carrito]
-    nuevoCarrito[index].cantidad > 1 ? nuevoCarrito[index].cantidad -= 1 : nuevoCarrito.splice(index, 1)
-    setCarrito([...nuevoCarrito])
-  }
-
-  const agregarNumeroCarrito = () =>{
-    return carrito.reduce((acc, prod) => acc += prod.cantidad, 0)
-  }
 
 
   return (
     <>
-      <CartContext.Provider value={{carrito, setCarrito, agregarAlCarrito, agregarNumeroCarrito, agregar1AlCarrito, eliminarDeCarrito}}>
+      <CartProvider>
         <BrowserRouter>
           <Navbar/>
           <Routes>
@@ -58,8 +23,8 @@ function App() {
             <Route path='/checkout' element={<Carrito/>}/>
           </Routes>
         </BrowserRouter>
-      </CartContext.Provider>
 
+      </CartProvider>
     </>
   )
 }
