be3d57f3e2a65e96c2948a60586b4ba7442ed080
Franco Catania <francocatania93@gmail.com>
2023-08-25 21:07:02 -0300

    Se agregó una barra buscadora, se creo el componente BarraBuscadora, Busqueda y FiltrosNav. Ademas se hizo la conexion a una nueva bd de firestore ya que se consumio toda la cuota debido a no poner la funcion getDocs de Busqueda dentro de un useEffect

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,15 +1,18 @@
+import { useParams } from "react-router-dom"
 import { Carousel } from "./Carousel"
 import { ItemList } from "./ItemList"
+import { Busqueda } from "./Busqueda"
 
 export const ItemListContainer = (props) =>{
-    // console.log(props)
+
     const ofertasCarousel = ["Hasta 6 cuotas sin interés", "Hacé ese regalo que tanto querés", "10% descuento en 3 cuotas sin interés"]
-    // console.log(ofertasCarousel)
+
     
     return (
         <div className="itemListContainer">
             <Carousel ofertas={ofertasCarousel}/>
             <ItemList/>
+
         </div>
     )
 }
