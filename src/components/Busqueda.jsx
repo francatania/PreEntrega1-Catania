03a4bfea4ac8a68977e2bb873eb34805cf89f580
Franco Catania <francocatania93@gmail.com>
2023-09-29 20:42:13 -0300

    Se arreglaron las media querys de las cards y spinner de busqueda

--- src/components/Busqueda.jsx
+++ src/components/Busqueda.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom"
 import { Item } from "./Item"
 import { collection, getDocs} from "firebase/firestore"
 import { db } from "../firebase/firebase"
+import ClipLoader from "react-spinners/ClipLoader";
 
 
 export function Busqueda(){
@@ -22,33 +23,60 @@ export function Busqueda(){
                     return {...prod.data(), id: prod.id}
                 })
             )
-            setIsLoading(false)
+            setTimeout(()=>{
+                setIsLoading(false)
+            },400)
         })
     }, [])
+
+    const override = {
+        display: "flex",
+        justifyContent: "center", 
+        alignItems: "center",     
+        height: "100vh",          
+    };
 
     const resultado = prendas.filter(producto=> producto.nombre.toLowerCase().includes(busqueda.busqueda))
     
     return <>
-        <div className="itemListSearch">
-            <div className="itemListSearch__tituloBusquedaContainer">
-                <h2>Resultado de búsqueda</h2>
-            </div>
-            {isLoading ? <div className="itemListSearch__loading">
-                <h3>Cargando...</h3>
-            </div> 
-            :resultado.length > 0 ? <div className="itemList">
-                                                {resultado.map((prenda) =>{
-                                                    return <Item key={prenda.id} img={prenda.img} img2={prenda.img2} nombre={prenda.nombre} precio={prenda.precio} id={prenda.id} agregar ={()=> agregar1AlCarrito(prenda, 1)} />
-                                                })}
-                                    </div>
-                                    : 
-                                    <div className="itemListSearch__notFoundContainer">
-                                        <h3>No se encontraron productos para "{busqueda.busqueda}"</h3>
-                                    </div>
-                                    }
-                    
+        {isLoading ?
             
+                <div className="itemList itemList__itemList-search">
+                    <div className="itemList itemList__loading">
+                    <h3>Cargando</h3>
+                        <ClipLoader
+                            color={'#3e2f5b'}
+                            loading={isLoading}
+                            css={override}
+                            size={30}
+                        />
+                    </div>
+                </div> 
+            
+            :
 
-        </div>
+           <div className="itemListSearch">
+           <div className="itemListSearch__tituloBusquedaContainer">
+               <h2>Resultado de búsqueda</h2>
+           </div>
+ 
+           {resultado.length > 0 ? <div className="itemList">
+                                               {resultado.map((prenda) =>{
+                                                   return <Item key={prenda.id} img={prenda.img} img2={prenda.img2} nombre={prenda.nombre} precio={prenda.precio} id={prenda.id} agregar ={()=> agregar1AlCarrito(prenda, 1)} />
+                                               })}
+                                   </div>
+                                   : 
+                                   <div className="itemListSearch__notFoundContainer">
+                                       <h3>No se encontraron productos para </h3>
+                                       <h3>"{busqueda.busqueda}"</h3>
+                                   </div>}
+                                   
+                   
+           
+
+       </div>
+           }
+
+        
     </>
 }
